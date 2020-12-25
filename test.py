ed456ae4a9dd4bbb5666e066ebc3e181a9d359bb
Hannes <45782468+Dimbright@users.noreply.github.com>
2020-12-25 16:51:46 +0800

    Update test.py
    add function: evaluate vote model

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import sys
+
 from utils import load_model, extend_maps, prepocess_data_for_lstmcrf
 from data import build_corpus
 from evaluating import Metrics
@@ -11,7 +13,12 @@ BiLSTMCRF_MODEL_PATH = './ckpts/bilstm_crf.pkl'
 REMOVE_O = False  # 在评估的时候是否去除O标记
 
 
-def main():
+def main(output_to_file):
+    if output_to_file:
+        print('正在生成测试结果...')
+        __stdout__ = sys.stdout
+        sys.stdout = open('./test/test_log.txt', 'a', encoding='utf-8')
+
     print("读取数据...")
     train_word_lists, train_tag_lists, word2id, tag2id = \
         build_corpus("train")
@@ -53,11 +60,24 @@ def main():
     metrics.report_scores()
     metrics.report_confusion_matrix()
 
+    print("加载并评估vote模型...")
     ensemble_evaluate(
         [hmm_pred, crf_pred, lstm_pred, lstmcrf_pred],
         test_tag_lists
     )
 
+    print("加载并评估dictionary+vote模型...")
+    dict_pred = []
+    metrics = Metrics(target_tag_list, dict_pred, remove_O=REMOVE_O)
+    metrics.report_scores()
+    metrics.report_confusion_matrix()
 
+    if output_to_file:
+        sys.stdout.close()
+        sys.stdout = __stdout__
+        print('已生成测试结果到\'./test/test_log.txt\'')
+        
+        
 if __name__ == "__main__":
-    main()
+    output = True
+    main(output)
